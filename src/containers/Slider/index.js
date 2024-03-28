1730ac469527d300c95e3ca5444134dafb67e887
Laban G <guildeur@hotmail.fr>
2024-03-28 09:31:34 +0100

    restructured Slider, added id to object of the Slider in the Json

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,53 +5,49 @@ import { displayMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-      new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const timeoutId = useRef(null);
-  const nextCard = () => {
-      setTimeout(
-          () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
-          5000
-      ); // probleme length undefine on first slide
-  };
-  useEffect(() => {
-    clearTimeout(timeoutId.current);
-    timeoutId.current = setTimeout(() => {
-      nextCard();
-    }, 5000);
-  }, [index]);
-  
-  const handleRadioChange = (currentIndex) => {
-    setIndex(currentIndex);
-  };
+    const { data } = useData();
+    const [index, setIndex] = useState(0);
+    const byDateDesc = data?.focus?.sort((evtA, evtB) =>
+        new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    ) || [];
+    const timeoutId = useRef(null);
+    const nextCard = () => {
+        setIndex((prevIndex) => (prevIndex === byDateDesc.length - 1 ? 0 : prevIndex + 1));
+    };
+    useEffect(() => {
+        clearTimeout(timeoutId.current);
+        timeoutId.current = setTimeout(() => {
+            nextCard();
+        }, 5000);
+    }, [index]);
 
-  return (
-      <div className="SlideCardList">
-          {byDateDesc?.map((event, idx) => (
-              <div
-                  key={event.title}
-                  className={`SlideCard SlideCard--${
-                      index === idx ? "display" : "hide"
-                  }`}
-              >
-                  <img src={event.cover} alt="forum" />
-                  <div className="SlideCard__descriptionContainer">
-                      <div className="SlideCard__description">
-                          <h3>{event.title}</h3>
-                          <p>{event.description}</p>
-                          <div>{displayMonth(new Date(event.date))}</div>
-                      </div>
-                  </div>
-              </div>
-          ))}
-          <div className="SlideCard__paginationContainer">
+    const handleRadioChange = (currentIndex) => {
+        setIndex(currentIndex);
+    };
+
+    return (
+        <div className="SlideCardList">
+            {byDateDesc.map((event, idx) => (
+                <div
+                    key={event.id}
+                    className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
+                        }`}
+                >
+                    <img src={event.cover} alt="forum" />
+                    <div className="SlideCard__descriptionContainer">
+                        <div className="SlideCard__description">
+                            <h3>{event.title}</h3>
+                            <p>{event.description}</p>
+                            <div>{displayMonth(new Date(event.date))}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className="SlideCard__paginationContainer">
                 <div className="SlideCard__pagination">
-                    {byDateDesc?.map((event, radioIdx) => (
+                    {byDateDesc.map((event, radioIdx) => (
                         <input
-                            key={`bulletPoint-${event.title}`}
+                            key={event.id}
                             type="radio"
                             name="radio-button"
                             checked={index === radioIdx}
@@ -60,8 +56,8 @@ const Slider = () => {
                     ))}
                 </div>
             </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Slider;
